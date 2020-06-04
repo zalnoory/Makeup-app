0ee930e1412af15ff3b2dfbd8744ee8f64de1370
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-06-03 21:15:39 -0500

    updated files

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 import { getBrands, getProductTag } from '../services/productsService'
+import imageLoader from '../services/images'
 import ListGroup from './common/listGroup'
-import Pagination from './common/pagination'
-import Loader from './loader'
+import Pagination from './common/page-pagination'
+import ProductsPagination from './common/products-pagination'
+import Loader from './common/loader'
+import Navbar from './navbar'
 import dataPagination from '../utils/data-pagination'
+import './../style/products-component.css'
 
 class Products extends React.Component {
   state = {
@@ -12,13 +16,14 @@ class Products extends React.Component {
     products: [],
     brands: [],
     tags: [],
+    images: [],
     selectedBrand: 'All Brands',
     selectedTag: 'All Tags',
     pageSize: 40,
     currentPage: 1,
   }
 
-  /*connect to server */
+  /*connect to backend*/
   async componentDidMount() {
     const { data } = await axios.get(
       'https://zahrah-products.s3.us-east-2.amazonaws.com/products.json'
@@ -32,6 +37,7 @@ class Products extends React.Component {
       products: data.products,
       brands,
       tags: getProductTag(),
+      images: imageLoader(),
       isLoading: false,
     })
   }
@@ -78,73 +84,160 @@ class Products extends React.Component {
       products,
       brands,
       tags,
+      images,
       selectedBrand,
       selectedTag,
       pageSize,
       currentPage,
+      isLoading,
     } = this.state
 
     const filtered = this.filterLists()
 
     const productsPagination = dataPagination(filtered, pageSize, currentPage)
 
-    return this.state.isLoading ? (
-      <Loader />
-    ) : (
-      <div className="row">
-        <div className="col-3">
-          <ListGroup
-            brands={brands}
-            tags={tags}
-            selectedBrand={selectedBrand}
-            selectedTag={selectedTag}
-            onBrandSelect={this.handleBrandSelect}
-            onTagSelect={this.handleTagSelect}
-          />
-        </div>
-        <div className="col">
-          <p style={{ textAlign: 'center' }}> {filtered.length} items</p>
-          <div className="container">
-            <ul className="list-inline">
-              {productsPagination.length === 0 ? (
-                <p> Sorry, no item is found.</p>
-              ) : (
-                productsPagination.map((product) => (
-                  <li
-                    className="list-inline-item m-2"
-                    style={{ cursor: 'pointer' }}
-                    key={product.id}
-                  >
-                    <div className="container">
-                      <img
-                        className="m-2"
-                        style={{ maxWidth: '125px', maxHeight: '125px' }}
-                        src={product.api_featured_image}
-                        alt={product.name}
-                      />
-                      <div>
-                        <div className="m-2">
-                          <span className="m-1">{product.brand}</span>
-                          <span className="m-1">{product.category}</span>
-                          <span>{product.product_type}</span>
+    return (
+      <React.Fragment>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="row">
+            <div className="col-3">
+              <ListGroup
+                brands={brands}
+                tags={tags}
+                selectedBrand={selectedBrand}
+                selectedTag={selectedTag}
+                onBrandSelect={this.handleBrandSelect}
+                onTagSelect={this.handleTagSelect}
+              />
+            </div>
+            <div className="col">
+              <Navbar images={images} />
+              <p
+                style={{
+                  textAlign: 'center',
+                  fontSize: '10px',
+                  fontWeight: '500',
+                  fontFamily: 'Brandon Text',
+                  marginTop: '50px',
+                }}
+              >
+                {' '}
+                {filtered.length} items
+              </p>
+              <div className="container">
+                <ProductsPagination productsPagination={productsPagination} />
+                {/* <ul className="list-inline">
+                  {productsPagination.length === 0 ? (
+                    <p> Sorry, no item is found.</p>
+                  ) : (
+                    productsPagination.map((product) => (
+                      <li
+                        className="list-inline-item m-2"
+                        style={{ cursor: 'pointer' }}
+                        key={product.id}
+                      >
+                        <div className="container">
+                          <img
+                            className="m-2"
+                            style={{ maxWidth: '125px', maxHeight: '125px' }}
+                            src={product.api_featured_image}
+                            alt={product.name}
+                          />
+                          <div>
+                            <div className="m-2">
+                              <span className="m-1">{product.brand}</span>
+                              <span>{product.product_type}</span>
+                              <div style={{textAlign:'center'}}>
+                                <span class="dot"></span>
+                                <span class="dot"></span>
+                                <span class="dot"></span>
+                                <span class="dot"></span>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  </li>
-                ))
-              )}
-            </ul>
-            <Pagination
-              productsCount={filtered.length}
-              pageSize={pageSize}
-              currentPage={currentPage}
-              onPageChange={this.handlePageChange}
-            />
+                      </li>
+                    ))
+                  )}
+                </ul> */}
+                <Pagination
+                  productsCount={filtered.length}
+                  pageSize={pageSize}
+                  currentPage={currentPage}
+                  onPageChange={this.handlePageChange}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        )}
+      </React.Fragment>
     )
   }
 }
 
 export default Products
+
+// return (
+//   <React.Fragment>
+//     {isLoading ? (
+//       <Loader />
+//     ) : (
+//       <div className="row">
+//         <div className="col-3">
+//           <ListGroup
+//             brands={brands}
+//             tags={tags}
+//             selectedBrand={selectedBrand}
+//             selectedTag={selectedTag}
+//             onBrandSelect={this.handleBrandSelect}
+//             onTagSelect={this.handleTagSelect}
+//           />
+//         </div>
+//         <div className="col">
+
+//           <Navbar images={images} />
+//           <p style={{ textAlign: 'center' }}> {filtered.length} items</p>
+//           <div className="container">
+//             <ul className="list-inline">
+//               {productsPagination.length === 0 ? (
+//                 <p> Sorry, no item is found.</p>
+//               ) : (
+//                 productsPagination.map((product) => (
+//                   <li
+//                     className="list-inline-item m-2"
+//                     style={{ cursor: 'pointer' }}
+//                     key={product.id}
+//                   >
+//                     <div className="container">
+//                       <img
+//                         className="m-2"
+//                         style={{ maxWidth: '125px', maxHeight: '125px' }}
+//                         src={product.api_featured_image}
+//                         alt={product.name}
+//                       />
+//                       <div>
+//                         <div className="m-2">
+//                           <span className="m-1">{product.brand}</span>
+//                           {/* <span className="m-1">{product.category}</span> */}
+//                           <span>{product.product_type}</span>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </li>
+//                 ))
+//               )}
+//             </ul>
+//             <Pagination
+//               productsCount={filtered.length}
+//               pageSize={pageSize}
+//               currentPage={currentPage}
+//               onPageChange={this.handlePageChange}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     )}
+//   </React.Fragment>
+// )
--- src/utils/data-pagination.js
+++ src/utils/data-pagination.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 const dataPagination = (items, pageSize, pageNumber) => {
   const startingIndex = (pageNumber - 1) * pageSize
   const lastIndex = pageNumber * pageSize
