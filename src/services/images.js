0799bc17e287d415cec6e2a0a55a24dc6d1d5642
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-06-03 21:22:10 -0500

    add files

---
+++ src/services/images.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    id: 1,
+    src: 'karly-jones-xBqYLnRhfaI-unsplash.jpg',
+    title: 'Foundation',
+    description: 'Makeup-fdation',
+  },
+  {
+    id: 2,
+    src: 'karly-jones-K3p8QlzBf7M-unsplash.jpg',
+    title: 'Lipstick',
+    description: 'Makeup-lip',
+  },
+  {
+    id: 3,
+    src: 'jake-peterson-NZUYV-NezV0-unsplash.jpg',
+    title: 'Eyeshadow',
+    description: 'makeup-eye',
+  },
+  {
+    id: 4,
+    src: 'caroline-attwood-E1rH__X9SA0-unsplash.jpg',
+    title: 'Blush',
+    description: 'makeup-blush',
+  },
+]
+
+function imageLoader() {
+  return images
+}
+
+export default imageLoader
