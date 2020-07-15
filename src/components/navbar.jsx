99c91110d658fa23b8e3ebe6a8a46e48b568dce5
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-07-15 10:10:16 -0500

    updated navbar.jsx file

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,40 +5,21 @@ const Navbar = (props) => {
   const { images } = props
 
   return (
-    <div className="container">
-      <ul className="nav">
-        {images.map((image) => (
-          <li key={image.id} className=" nav-item">
-            <a className="nav-link active" href="#">
-              <img
-                className="navbar-images"
-                src={require(`../images/${image.src}`)}
-                alt={image.description}
-              />
-            </a>
-            {image.title}
-          </li>
-        ))}
-      </ul>
+    <div className="top-nav-container">
+      {images.map((image) => (
+        <div key={image.id} className=" div-item">
+          <a className="top-nav-link" href="#">
+            <img
+              className="nav-images"
+              src={require(`../images/${image.src}`)}
+              alt={image.description}
+            />
+          </a>
+          <p>{image.title}</p>
+        </div>
+      ))}
     </div>
   )
 }
 
 export default Navbar
-
-{
-  /* <ul className="nav justify-content">
-      {images.map((image) => (
-        <li key={image.id} className="ml-0 nav-item">
-          <a className="nav-link active" href="#">
-            <img
-              className="navbar-images"
-              src={require(`../images/${image.src}`)}
-              alt={image.description}
-            />
-          </a>
-          {image.title}
-        </li>
-      ))}
-    </ul> */
-}
