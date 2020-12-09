e74582124e9dca41a257965b1d77c0df5809b1c9
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-12-08 19:49:52 -0600

    committing the delete

--- src/utils/data-pagination.js
+++
@@ -1,7 +0,0 @@
-const dataPagination = (items, pageSize, pageNumber) => {
-  const startingIndex = (pageNumber - 1) * pageSize
-  const lastIndex = pageNumber * pageSize
-  return items.slice(startingIndex, lastIndex)
-}
-
-export default dataPagination
